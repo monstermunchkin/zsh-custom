cd009acd706ba3981728af2d43b9b2957412a12f
Thomas Hipp <thomashipp@gmail.com>
2012-09-06 09:44:10 +0200

    Added ctrl+<arrow> key bindings

--- key-bindings.zsh
+++ key-bindings.zsh
@@ -1,3 +1,5 @@
 bindkey '^R' history-incremental-search-backward
 bindkey "\e[7~" beginning-of-line # Home
 bindkey "\e[8~" end-of-line # End
+bindkey '^[[1;5C' emacs-forward-word # ctrl+right
+bindkey '^[[1;5D' emacs-backward-word # ctrl+left
