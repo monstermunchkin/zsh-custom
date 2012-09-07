83f37d0914ce86bc5d920f60270d4728f5d14790
Thomas Hipp <thomashipp@gmail.com>
2012-09-07 20:30:45 +0200

    Added more key bindings

--- key-bindings.zsh
+++ key-bindings.zsh
@@ -1,4 +1,7 @@
 bindkey '^R' history-incremental-search-backward
+bindkey "\e[3~" vi-backward-delete-char # delete
+bindkey "\e[5~" beginning-of-history # page up
+bindkey "\e[6~" end-of-history # page down
 bindkey "\e[7~" beginning-of-line # Home
 bindkey "\e[8~" end-of-line # End
 bindkey '^[[1;5C' emacs-forward-word # ctrl+right
