80ba32658873a690e51e4c1d7b5192b2939d8579
Thomas Hipp <thomashipp@gmail.com>
2012-10-01 08:58:59 +0200

    added time to prompt

--- aliases.zsh
+++ aliases.zsh
@@ -1,5 +1,9 @@
 alias vi='vim'
 alias sudo='sudo '
-alias mpsync='/home/thomas/Code/Python/mpsync.py config /home/thomas/Code/Python/mpsync.d/config2.json'
+alias mpsync='/home/thomas/code/python/mpsync.py config /home/thomas/code/python/mpsync.d/config2.json'
 alias pip_upgrade_all='pip3 freeze | cut -d= -f1 | parallel sudo pip3 install --upgrade'
 alias pip2_upgrade_all='pip2 freeze | cut -d= -f1 | parallel sudo pip2 install --upgrade'
+
+alias -s pdf='evince'
+alias -s png='ristretto'
+alias -s jpg='ristretto'
--- custom.zsh-theme
+++ custom.zsh-theme
@@ -1,5 +1,5 @@
 PROMPT=$'
-%{$fg_bold[blue]%}%/%{$reset_color%} $(git_prompt_info)%{$fg[white]%}[%n@%m]%{$reset_color%}
+%{$fg_bold[blue]%}%/%{$reset_color%} $(git_prompt_info)%{$fg[white]%}[%n@%m]%{$reset_color%} %{$fg[white]%}[%T]%{$reset_color%}
 %{$fg_bold[black]%}>%{$reset_color%} '
 
 PROMPT2="%{$fg_blod[black]%}%_> %{$reset_color%}"
