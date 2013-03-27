8443052085ec6eae97dd033fdb6c90684de84c95
Thomas Hipp <thomashipp@gmail.com>
2013-03-27 12:54:51 +0100

    zsh-custom: changed autocompletion behaviour
    Tab completion is broken when file names contain underscores.

--- completion.zsh
+++ completion.zsh
@@ -1,1 +1,10 @@
+# NOTE: https://github.com/robbyrussell/oh-my-zsh/issues/1398
+
+if [ "x$CASE_SENSITIVE" = "xtrue" ]; then
+  zstyle ':completion:*' matcher-list 'r:|=*'
+  unset CASE_SENSITIVE
+else
+  zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|=*'
+fi
+
 zstyle ':completion:*:commands' rehash 1
